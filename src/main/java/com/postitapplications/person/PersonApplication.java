afc152be02ba49f0b22cf6a6b67883115af044ef
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-07-30 10:38:16 +0100

    fixed cors for get and delete by id, added exception response body class for personExceptionHandler

--- src/main/java/com/postitapplications/person/PersonApplication.java
+++ src/main/java/com/postitapplications/person/PersonApplication.java
@@ -2,11 +2,24 @@ package com.postitapplications.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PersonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/person/**").allowedOrigins("http://localhost:8081");
+            }
+        };
     }
 }
--- src/main/java/com/postitapplications/person/exception/PersonExceptionHandler.java
+++ src/main/java/com/postitapplications/person/exception/PersonExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.postitapplications.person.exception;
 
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,25 +12,19 @@ public class PersonExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {NullOrEmptyException.class, NullPointerException.class,
         IllegalArgumentException.class})
     public ResponseEntity<Object> handleBadRequestException(Exception exception) {
-        Map<String, Object> body = new LinkedHashMap<>();
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(badRequest,
+            exception.getMessage());
 
-        body.put("error", badRequest);
-        body.put("message", exception.getMessage());
-        body.put("timestamp", LocalDateTime.now());
-
-        return new ResponseEntity<>(body, badRequest);
+        return new ResponseEntity<>(exceptionResponseBody, badRequest);
     }
 
     @ExceptionHandler(value = {PersonNotFoundException.class})
     public ResponseEntity<Object> handlePersonNotFoundException(PersonNotFoundException exception) {
-        Map<String, Object> body = new LinkedHashMap<>();
         HttpStatus notFound = HttpStatus.NOT_FOUND;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(notFound,
+            exception.getMessage());
 
-        body.put("error", notFound);
-        body.put("message", exception.getMessage());
-        body.put("timestamp", LocalDateTime.now());
-
-        return new ResponseEntity<>(body, notFound);
+        return new ResponseEntity<>(exceptionResponseBody, notFound);
     }
 }
