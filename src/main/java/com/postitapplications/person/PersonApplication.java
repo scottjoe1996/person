0962a32b65266ec4578b231b8677406a89946bee
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-08-28 17:07:59 +0100

    changed port number, and using eurekaDiscoveryClient

--- src/main/java/com/postitapplications/person/PersonApplication.java
+++ src/main/java/com/postitapplications/person/PersonApplication.java
@@ -2,12 +2,12 @@ package com.postitapplications.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PersonApplication {
 
